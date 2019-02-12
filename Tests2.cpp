b75048f3d99c7a432277c09d9438377eaf76f8fa
Hava-Kantrowitz <46357722+Hava-Kantrowitz@users.noreply.github.com>
2019-02-12 11:52:48 -0500

    Added tests

--- Tests2.cpp
+++ Tests2.cpp
@@ -34,21 +34,53 @@ bool Tests2::doTests()
 	bool ok2 = makeAntsTest();
 	//see whether they move
 	bool ok3 = antsMoveTest();
+	//tests steps to birth of ant
+	bool ok4 = testStepsToBirthAnt();
+	//test breed eligibility for ant
+	bool ok5 = testBreedEligibilityAnt();
 	//see whether they breed
-	bool ok4 = antsBreedTest();
+	bool ok6 = antsBreedTest();
 	//see whether they die?
-	bool ok5 = antsDieTest();//maybe if they wander off?
+	bool ok7 = antsDieTest();//maybe if they wander off?
 	//populate with doodlebugs
-	bool ok6 = makeDoodlesTest();
+	bool ok8 = makeDoodlesTest();
+	//see whether occupied square has prey
+	bool ok9 = testIsPrey();
+	//tests randomization
+	bool ok10 = testRandomization();
+	//test enumeration
+	bool ok11 = testEnumeration();
+	//test adjacency
+	bool ok12 = testAdjacency();
+	//test unoccupied
+	bool ok13 = testUnoccupied();
 	//see whether they move
-	bool ok7 = doodleMoveTest();
+	bool ok14 = doodleMoveTest();
+	//test number of steps calculator
+	bool ok15 = testNumSteps();
+	//test buggies eating ants
+	bool ok16 = testAntsEaten();
+	//tests creature removal
+	bool ok17 = testRemoval();
+	//test steps to birth for buggy
+	bool ok18 = testStepsBirthBuggy();
+	//test starvation state
+	bool ok19 = testStarvationState();
+	//test breed eligibility
+	bool ok20 = testBreedEligibilityBuggy();
 	//see whether they breed
-	bool ok8 = doodleBreedTest();
+	bool ok21 = doodleBreedTest();
 	//see whether they eat ants
-	bool ok9 = doodleEatTest();
+	bool ok22 = doodleEatTest();
 	//see whether they die
-	bool ok10 = doodleDietest();
-	results = ok1 && ok2 && ok3 && ok4 && ok5 && ok6 && ok7 && ok8 && ok9 && ok10;
+	bool ok23 = doodleDietest();
+	//test overall ant action works
+	bool ok24 = testAntAction();
+	//test overall buggy action works
+	bool ok25 = testBuggyAction();
+	results = ok1 && ok2 && ok3 && ok4 && ok5 && ok6 && ok7 && ok8 && ok9 && ok10
+			&& ok11 && ok12 && ok13 && ok14 && ok15 && ok16 && ok17 && ok18 && ok19
+			&& ok20 && ok21 && ok22 && ok23 && ok24 && ok25;
 	return results;
 }
 
@@ -147,6 +179,22 @@ bool Tests2::antsMoveTest()
 }
 
 /**
+ * Tests how many steps a given ant has until it can give birth
+ * @return true if test passes, false otherwise
+ */
+bool Tests2::testStepsToBirthAnt(){
+	return true;
+}
+
+/**
+ * Tests how whether an ant can breed
+ * @return true if test passes, false otherwise
+ */
+bool Tests2::testBreedEligibilityAnt(){
+	return true;
+}
+
+/**
  * Tests ant breeding function
  * @return true if test passes, false otherwise
  */
@@ -180,6 +228,46 @@ bool Tests2::makeDoodlesTest()
 }
 
 /**
+ * Tests whether an organism is prey, meaning it is an ant
+ * @return true if test passes, false otherwise
+ */
+bool Tests2::testIsPrey(){
+	return true;
+}
+
+/**
+ * Tests randomization of ant movement choice
+ * @return true if test passes, false otherwise
+ */
+bool Tests2::testRandomization(){
+	return true;
+}
+
+/**
+ * Tests enumeration of ant movement choice
+ * @return true if test passes, false otherwise
+ */
+bool Tests2::testEnumeration(){
+	return true;
+}
+
+/**
+ * Tests whether a cell is adjacent to another cell
+ * @return true if test passes, false otherwise
+ */
+bool Tests2::testAdjacency(){
+	return true;
+}
+
+/**
+ * Tests whether a cell is unoccupied
+ * @return true if test passes, false otherwise
+ */
+bool Tests2::testUnoccupied(){
+	return true;
+}
+
+/**
  * Tests doodle moving function
  * @return true if test passes, false otherwise
  */
@@ -190,6 +278,54 @@ bool Tests2::doodleMoveTest()
 
 
 	return result;
+}
+
+/**
+ * Tests current number of steps of organism
+ * @return true if test passes, false otherwise
+ */
+bool Tests2::testNumSteps(){
+	return true;
+}
+
+/**
+ * Tests if a buggy ate an ant
+ * @return true if test passes, false otherwise
+ */
+bool Tests2::testAntsEaten(){
+	return true;
+}
+
+/**
+ * Tests removal from grid of an organism
+ * @return true if test passes, false otherwise
+ */
+bool Tests2::testRemoval(){
+	return true;
+}
+
+/**
+ * Tests how many steps a given buggy has until it can give birth
+ * @return true if test passes, false otherwise
+ */
+bool Tests2::testStepsBirthBuggy(){
+	return true;
+}
+
+/**
+ * Tests if a buggy is starving
+ * @return true if test passes, false otherwise
+ */
+bool Tests2::testStarvationState(){
+	return true;
+}
+
+/**
+ * Tests if a buggy is eligible to give birth
+ * @return true if test passes, false otherwise
+ */
+bool Tests2::testBreedEligibilityBuggy(){
+	return true;
 }
 
 /**
@@ -223,6 +359,22 @@ bool Tests2::doodleDietest()
 	bool result = true;
 	std::cout << "Running the doodlebug dies test" << std::endl;
 	return result;
+}
+
+/**
+ * Tests that ants are able to take an action
+ * @return true if test passes, false otherwise
+ */
+bool Tests2::testAntAction(){
+	return true;
+}
+
+/**
+ * Tests that buggies are able to take an action
+ * @return true if test passes, false otherwise
+ */
+bool Tests2::testBuggyAction(){
+	return true;
 }
 
 /**
--- Tests2.h
+++ Tests2.h
@@ -15,13 +15,28 @@ public:
 	bool gridTest();
 	bool makeAntsTest();
 	bool antsMoveTest();
+	bool testStepsToBirthAnt();
+	bool testBreedEligibilityAnt();
 	bool antsBreedTest();
 	bool antsDieTest();
 	bool makeDoodlesTest();
+	bool testIsPrey();
+	bool testRandomization();
+	bool testEnumeration();
+	bool testAdjacency();
+	bool testUnoccupied();
 	bool doodleMoveTest();
+	bool testNumSteps();
+	bool testAntsEaten();
+	bool testRemoval();
+	bool testStepsBirthBuggy();
+	bool testStarvationState();
+	bool testBreedEligibilityBuggy();
 	bool doodleBreedTest();
 	bool doodleEatTest();
 	bool doodleDietest();
+	bool testAntAction();
+	bool testBuggyAction();
 
 
 	virtual ~Tests2();
